f3206eca335e1a00754fc9544a0b5f3d7a08ca5b
macnemoto <marcosj465@gmail.com>
2023-01-20 20:08:55 -0400

    :lipstick: last fix session

--- src/lib/sessions.svelte
+++ src/lib/sessions.svelte
@@ -3,17 +3,18 @@
 	import start from '../lib/img/HeroCrypto.svg';
 </script>
 
-<div class="mt-16 px-5 flex sm:mx-20 sm:mt-20 sm:px-6 2xl:mx-64">
+<div class="mt-16 px-5 flex flex-col lg:flex lg:flex-row lg:mx-20 lg:mt-20 lg:px-6 2xl:mx-64">
 	<div class="flex flex-col justify-center items-center sm:pr-5">
-		<h1 class="text-4xl text-white text-center font-bold mb-3">Compra criptomonedas sin pagar comisiones</h1>
+		<h1 class="text-4xl text-white text-center font-bold mb-3">
+			Compra criptomonedas sin pagar comisiones
+		</h1>
 		<p class="text-center text-slate-600">
 			Compra fácilmente bitcoins, USDT y otras criptomonedas en menos de 7 minutos y recíbelas en
 			cualquier parte del mundo.
 		</p>
-        <Button gradient color="purpleToBlue" class="mt-5 py-4">Registrarme como comprador</Button>
-
+		<Button gradient color="purpleToBlue" class="mt-5 py-4">Registrarme como comprador</Button>
 	</div>
-	<div>
-		<img class="mt-4" src={start} alt="startImg" />
+	<div class="flex justify-center items-center">
+		<img class="mt-4 sm:max-w-md lg:max-w-sm xl:max-w-lg 2xl:max-w-6xl " src={start} alt="startImg" />
 	</div>
 </div>

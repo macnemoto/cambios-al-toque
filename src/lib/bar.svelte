a099692c072b92574de34c4ff541997aab9e3b9f
macnemoto <marcosj465@gmail.com>
2023-01-21 00:09:55 -0400

    :art: bar session

---
+++ src/lib/bar.svelte
@@ -0,0 +1,14 @@
+<div class="grid gap-4 grid-cols-3 grid-rows-3 justify-items-center bg-sky-900 mt-10">
+    <div class="col-span-1 flex items-center">
+        <h3 class="text-2xl text-white">+30.000</h3>
+        <p class="text-gray-400">Bitcoins vendidos</p>
+    </div>
+    <div class="col-span-1 flex items-center">
+        <h3 class="text-2xl text-white">+4</h3>
+        <p class="text-gray-400">Años de experiencia</p>
+    </div>
+    <div class="col-span-1 flex items-center">
+        <h3 class="text-2xl text-white">+200M USD</h3>
+        <p class="text-gray-400">Transados</p>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Header from '$lib/header.svelte';
-    import StartSession from "$lib/sessions.svelte"
+	import StartSession from '$lib/sessions.svelte';
+	import Bar from '$lib/bar.svelte';
 </script>
 
 <header>
 	<Header />
-<StartSession/>
+	<StartSession />
+	<Bar />
 	<div />
 </header>
